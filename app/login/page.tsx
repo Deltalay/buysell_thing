996fbedd834d05b3f0b58e2822a9d039d4c82367
Deltalay <paylayhok168@gmail.com>
2024-04-30 16:07:33 +0700

    Update Dependencies

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,21 @@
 import { db } from "@/db/drizzle";
 import LoginForm from "../components/loginForm";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export default function LoginPage() {
   async function LoginAccount(formData: FormData) {
     "use server";
     const email = formData.get("email");
-    const password = formData.get('password');
-    
+    const password = formData.get("password");
+    if (email?.toString().length == 0 || email?.toString() == undefined ) {
+      return;
+    }
+    if (password?.toString().length == 0 || password?.toString().length == undefined) {
+      return;
+    }
+    console.log(email.toString())
+    console.log(await db.select().from(users).where(eq(users.email, email.toString())));
   }
   return (
     <section>
