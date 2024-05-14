1da349a6d1b4160ba59bf2e3ac01eb0fbed76049
Deltalay <paylayhok168@gmail.com>
2024-05-14 21:37:19 +0700

    Just little change

--- app/components/itemCard.tsx
+++ app/components/itemCard.tsx
@@ -1,12 +1,34 @@
+import Image from "next/image";
 interface ItemCardProps {
   price: string;
   image: string;
   content: string;
 }
-export default function ItemCard({price, image, content}: ItemCardProps) {
-  return <div className="bg-white hover:-translate-y-1 transition-transform ease-in duration-200 px-2 py-1 ">
-    <div>
-      <h1>Hello</h1>
+export default function ItemCard({ price, image, content }: ItemCardProps) {
+  return (
+    <div className="bg-white text-black hover:-translate-y-1 transition-transform ease-in duration-200 ">
+      <div>
+        <figure>
+          <Image
+            width={600}
+            height={400}
+            src="https://placehold.co/600x400/png"
+            alt="Shoes"
+          />
+        </figure>
+      </div>
+      <div className="mt-2 px-2 py-1">
+        <h2 className="text-xl font-semibold">Title</h2>
+        <p>{content}</p>
+        <div className="flex mt-2 justify-between items-center">
+          <div>
+            <p className="">{price}</p>
+          </div>
+          <div>
+            <button className="border-black border transition ease-in duration-150 hover:bg-black hover:text-white px-4 py-2 rounded-lg text-lg font-semibold">Buy</button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,20 @@ import ItemCard from "./components/itemCard";
 import NavBar from "./components/navbar";
 
 export default function Home() {
-  return <>
-  <NavBar/>
-  <section className="my-3">
-    <ItemCard price="22" content="32" image="da" />
-  </section>
-  </>
+  return (
+    <>
+      <NavBar />
+      <section className="my-3 grid grid-cols-4 gap-5 px-2">
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+        <ItemCard price="22" content="32" image="da" />
+      </section>
+    </>
+  );
 }
